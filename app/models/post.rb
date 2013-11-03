e4096eb97f7a153e9799203a575038db09752505
Luis Briones <lbriones.tic@gmail.com>
2013-11-03 20:05:17 -0300

    modificacion completa de vistas

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
   attr_accessible :descripcion, :funciones, :requisitos, :sueldo, :titulo
+
+  def self.search(search)
+  if search
+    find(:all, :conditions => ['titulo LIKE ?', "%#{search}%"])
+  else
+    find(:all)
+  end
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -77,7 +77,7 @@ PraxisWeb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'login#index'
+   root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
