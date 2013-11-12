56a4f062ab9a56c18306afbc4acbfe45d207bc24
Luis Briones <lbriones.tic@gmail.com>
2013-11-12 00:51:38 -0300

    incluye surveyor, importacion y configuracion sqlite3 y mysql2

--- Gemfile
+++ Gemfile
@@ -6,8 +6,9 @@ gem 'rails', '3.2.14'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-gem 'mysql2'
+#gem 'mysql2'
 gem 'devise'
+gem 'surveyor'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 PraxisWeb::Application.initialize!
 #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
